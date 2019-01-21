cb3f5ca4cb1810c8b896c45d04b086c038d39eb0
Dan Sensecall <dan@sensecall.co.uk>
2019-01-21 15:31:25 +0000

    moved to sprint structure

--- app/views/approve/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = new express.Router()
-
-router.use(/\/provider/, (req, res, next) => {
-  require(`./provider/routes`)(req, res, next);
-})
-
-module.exports = router
