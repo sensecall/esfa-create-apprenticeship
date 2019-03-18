735b12bc70764c88b945b2d6c75b1143921076ee
roblangdongov <rob.langdon@digital.education.gov.uk>
2019-03-18 08:54:33 +0000

    Sprint 5 setup

---
+++ app/views/approve/sprint-05/research/employers/routes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = new express.Router()
+
+var _ = require('underscore');
+var moment = require('moment');
+const cryptoRandomString = require('crypto-random-string');
+
+router.get('/', (req, res) => {
+	res.redirect(`/${req.feature}/email`)
+})
+
+//review an apprentice
+router.post('/review-an-apprentice', (req, res) => {
+	if (req.session.data['approve'] == 'yes') {
+	  res.redirect('confirmation')
+	} else {
+	  res.redirect('not-confirmed')
+	}
+  })
+  
+  module.exports = router
