0e4657dfd07a1e915713ddcdd1cad1b5a3cff396
Dan Sensecall <dan@sensecall.co.uk>
2019-05-09 15:40:37 +0100

    new kit

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage apprenticeships',
+  serviceName: 'Service name goes here',
 
   // Default port that prototype runs on
   port: '3000',
